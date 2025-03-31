10d4899f470e717992abf1314fab4c6875de8ecc
emmajudd <emmajudd@udel.edu>
2025-03-31 15:51:08 -0400

    put questions (basic and detailed)

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { useNavigate} from "react-router-dom"
+
 import "./Homepage.css"; // Import CSS for styling
 
 const HomePage = () => {
+  const navigate = useNavigate();
   return (
     <div className="homepage">
       <h1>The Career Helpi</h1>
@@ -11,14 +14,18 @@ const HomePage = () => {
         <section className="basic-questions">
           <h2>Basic Questions</h2>
           <p>Answer basic questions to help you find your ideal career.</p>
-          <button>Go to Basic Questions</button>
+          <button onClick={() => navigate("/basic-assessment")}>
+            Go to Basic Questions
+          </button>
         </section>
         
         {/* Detailed Questions */}
         <section className="detailed-questions">
           <h2>Detailed Questions</h2>
           <p>Answer more detailed questions to help you find your ideal career.</p>
-          <button>Go to Detailed Questions</button>
+          <button onClick={() => navigate("/detailed-assessment")}>
+            Go to Detailed Questions
+          </button>
         </section>
       </div>
 
