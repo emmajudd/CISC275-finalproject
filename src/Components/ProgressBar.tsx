72c6e65ec909d6cd5341fdc505bab2860a117ed1
ssmitty <ssmitty@udel.edu>
2025-04-27 15:05:01 -0400

    Added a sticky progress bar and fixed popups when submitting answers

--- src/Components/ProgressBar.tsx
+++ src/Components/ProgressBar.tsx
@@ -12,7 +12,7 @@ const ProgressBar: React.FC<ProgressBarProps> = ({ progress }) => {
     <div className="progress-container">
       <div
         className="progress-bar"
-        style={{ width: `${roundedProgress}%` }} // Dynamically set the width
+        //style={{ width: `${roundedProgress}%` }} // Dynamically set the width
       >
         <span className="progress-text">{`${roundedProgress}%`}</span> {/* Display rounded progress */}
       </div>
